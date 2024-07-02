ec05898a65f6048a98e7051702e4c2efd9f033da
GiovanaRods <gigi.hinata13@gmail.com>
2024-07-02 13:07:05 -0300

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Home from './paginas/home/Home';
+
 
 function App() {
-  const [valor, setValor] = useState(0);
-  function handleClick() {
-    setValor(valor + 1);
-  }
+
 
   return (
-    <div>
-      <Home
-        title="Componente Home"
-        description="Este é um componente Home que recebe props." />
-      <p>O valor é: {valor}</p>
-      <button onClick={handleClick}>Adicionar 1</button>
-    </div>
-  );
+    <>
+    <h1 className="text-3xl font-bold text-red-500 underline text-center">
+      Hello world!
+    </h1>
+    </>
+);
 }
 
 export default App;
